9dd23f4b06b79fd08f8bcb033419c3517d10b39c
Retinazer <coleyoung@protonmail.com>
2020-01-28 21:10:36 -0500

    Added more required dependencies

--- packet-tracer-7.3.0-install.sh
+++ packet-tracer-7.3.0-install.sh
@@ -8,20 +8,14 @@ PTFileCheck() {
 }
 
 getDeps() {
-	for ((i=0; i<${#deps[*]}; i++)); do
-		if [ $(echo ${deps[i]} | grep "deb") ]; then
-			echo "Grabbing ${deps[i]} ..."
-			for ((x=0; x<${#mirrors[*]}; x++)); do
-				echo ${mirrors[x]} | grep ${deps[i]} | xargs -I "LINK" wget -q "LINK"
-			done
-		else
-			installDeps ${deps[i]}
-		fi
+	for ((i=0; i<${#mirrors[*]}; i++)); do
+		echo "Grabbing ${mirrors[i]} ..."
+		wget -q ${mirrors[i]}
 	done
 }
 
 installDeps() {
-	if [ $(echo $1 | grep "deb") ]; then
+	if [ $(ls ./  | grep "$1") ]; then
 		echo "installing $1..."
 		ls ./ | grep $1 | xargs -I "PACKAGE" apt-get -qy --allow-downgrades install ./"PACKAGE" >> /dev/null
 	else
@@ -32,28 +26,6 @@ installDeps() {
 	fi
 }
 
-checkSum() {
-	for ((i=0; i<${#deps[*]}; i++)); do
-		if [ $(echo ${deps[i]} | grep "deb") ]; then
-			echo "Checking file integrity of ${deps[i]} ..."
-			if [ $(shasum ${deps[i]} | shasum -c | grep -o "OK") ]; then
-				installDeps ${deps[i]}
-			else
-			   echo "The checksum of ${deps[i]} did not match. The file may have been compromised or corrupted."
-			   printf "\nWould you like to continue with this installation? "
-			   read -p "[Y/N] " continueInvalid
-			   case $continueInvalid in
-				   [yY]* ) installDeps ${deps[i]}
-					   break;;
-				   [nN]* ) rm ${deps[i]}
-					   exit;;
-				   * ) exit;;
-			   esac
-			fi
-		fi
-	done
-}
-
 installPT() {
 	printf "\n\nInstalling Packet Tracer 7.3.0 ...\n"
 	dpkg -i $(ls | grep PacketTracer*.deb | head -n 1)
@@ -62,9 +34,9 @@ installPT() {
 cleanUp() {
 	printf "\nCleaning up after install ...\n"
 	for ((i=0; i<${#deps[*]}; i++)); do
-		if [ $(echo ${deps[i]} | grep "deb") ]; then
+		if [ $(ls ./  | grep ${deps[i]}) ]; then
 			echo "Removing ${deps[i]} ..."
-			rm ${deps[i]}
+			rm $(ls ./  | grep ${deps[i]})
 		fi
 	done
 }
@@ -77,19 +49,38 @@ checkInstalled() {
 	fi
 }
 
+<< COMMENT
+Below is where you can place additional dependencies.
+Be sure to use the correct name of the package and if
+it needs to be downloaded from an outside source, place
+it's download link in the "mirrors" array.
+
+Thank you Cisco for forcing me to create a tool that will let
+people use your product on GNU/Linux operating systems.
+COMMENT
+
 deps=(
 	wget
-	libdouble-conversion1_2.0.1-4ubuntu1_amd64.deb
-	qt-at-spi_0.4.0-3_amd64.deb
+	libnss3
+	libxslt1
+	libqt5webkit5
+	libdouble-conversion1
+	qt-at-spi
+	libjpeg-turbo8
+	libicu60
+	libssl1.0.0
 )
 
 mirrors=(
 	http://mirrors.kernel.org/ubuntu/pool/main/d/double-conversion/libdouble-conversion1_2.0.1-4ubuntu1_amd64.deb
 	http://mirrors.kernel.org/ubuntu/pool/main/q/qt-at-spi/qt-at-spi_0.4.0-3_amd64.deb
+	http://security.ubuntu.com/ubuntu/pool/main/libj/libjpeg-turbo/libjpeg-turbo8_1.4.2-0ubuntu3.3_amd64.deb
+	http://mirrors.kernel.org/ubuntu/pool/main/i/icu/libicu60_60.2-3ubuntu3_amd64.deb
+	http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.0.0_1.0.2g-1ubuntu4.15_amd64.deb
 )
 
 if [ `whoami` != root ]; then
-    echo "Please run this script as root or using sudo"
+    echo "Please run this script as root or using sudo."
     exit
 fi
 
@@ -110,11 +101,13 @@ case $readyToInstall in
    [yY]* )
 	   PTFileCheck
 	   getDeps
-	   checkSum
+		 for ((i=0; i<${#deps[*]}; i++)); do
+			 installDeps ${deps[i]}
+		 done
 	   installPT
 	   cleanUp
 	   checkInstalled
-	;;
+		 ;;
    [nN]* ) exit;;
    * ) exit;;
 esac
